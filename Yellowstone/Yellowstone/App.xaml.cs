b92b3ede49e20f25c472c9e2099925ad2e4f7316
Dmytro Bondarenko <bondarenkod@windowslive.com>
2018-06-30 23:12:03 +0300

    initial commit

---
+++ Yellowstone/Yellowstone/App.xaml.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization.Json;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Distribute;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Yellowstone
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			VersionTracking.Track();
+
+			var config = ReadConfiguration();
+
+			var appcentersharedsecret = config["appcenter_appsecret"].ToString();
+
+			AppCenter.Start($"ios={appcentersharedsecret};android={appcentersharedsecret};uwp={appcentersharedsecret}",
+				typeof(Analytics),
+				typeof(Crashes),
+				typeof(Distribute));
+
+			InitializeComponent();
+
+			MainPage = new MainPage();
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+
+		private JObject ReadConfiguration()
+		{
+			string file;
+			var assembly = typeof(App).GetTypeInfo().Assembly;
+
+			using (var stream = assembly.GetManifestResourceStream("Yellowstone.Resources.config.json"))
+			{
+				using (var reader = new StreamReader(stream))
+				{
+					file = reader.ReadToEnd();
+				}
+			}
+
+			var conf = JObject.Parse(file);
+
+			return conf;
+
+		}
+	}
+}
---
+++ Yellowstone/Yellowstone/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Yellowstone
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+			BindingContext = new MainPageViewModel();
+		}
+	}
+
+	public class MainPageViewModel
+	{
+		public ObservableCollection<Entry> Source { get; private set; }
+		public MainPageViewModel()
+		{
+			Source = new ObservableCollection<Entry>
+			{
+				new Entry("First time ever launched application", VersionTracking.IsFirstLaunchEver.ToString()),
+				new Entry("First time launching current version",
+					VersionTracking.IsFirstLaunchForCurrentVersion.ToString()),
+				new Entry("First time launching current build",
+					VersionTracking.IsFirstLaunchForCurrentBuild.ToString()),
+				new Entry("Current app version", VersionTracking.CurrentVersion),
+				new Entry("Current build", VersionTracking.CurrentBuild),
+				new Entry("Previous app version", VersionTracking.PreviousVersion),
+				new Entry("Previous app build", VersionTracking.PreviousBuild),
+				new Entry("First version of app installed", VersionTracking.FirstInstalledVersion),
+				new Entry("First build of app installed", VersionTracking.FirstInstalledBuild),
+				new Entry("List of versions installed", string.Join("; ", VersionTracking.VersionHistory)),
+				new Entry("List of builds installed", string.Join("; ", VersionTracking.BuildHistory))
+			};
+
+		}
+	}
+
+	public class Entry
+	{
+		public Entry(string title, string content)
+		{
+			Title = title;
+			Content = content;
+		}
+		public string Title { get; private set; }
+		public string Content { get; private set; }
+	}
+}
